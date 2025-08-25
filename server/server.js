19fda0f63df3a687a320d8beadce606df5829dd7
sneha-grg <snehagrg14@gmail.com>
2025-08-24 23:47:26 -0700

    setting up Express server with config

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb';
+
+const app = express();
+const port = process.env.PORT || 4000 // use the port in env, otherwise use 4000
+connectDB();
+
+app.use(express.json()); // parse incoming JSON req bodies
+app.use(cookieParser()); // add can  read cookies
+app.use(cors({credentials: true})) // allows browser to send cookies across domains/cookies to store JWTs
+
+app.get('/', (req,  res)=> res.send("API Working")); 
+
+app.listen(port, ()=> console.log(`Server started on Port: ${port}
+`))
